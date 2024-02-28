162bca094017e83d43c8e1001730ab8afffa90bd
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-02-28 11:27:05 +0700

    W03: Jawaban soal 3

--- 03-belajar-komponen/src/app/page.tsx
+++ 03-belajar-komponen/src/app/page.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import Profile from "../components/profile";
 import { Gallery } from "@/components/gallery";
+import ToDoList from "@/components/todolist";
+import Bio from "@/components/bio";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="justify-center mt-36">Ilmuwan yang istimewa</p>
       <Gallery/>
+      <ToDoList/>
+      <Bio/>
+    </main>
+  );
+}
+
       {/* <div className="flex mt-32">
       <Profile/>
       <div className="mx-96">
       <Profile/>
       </div>
       <Profile/>
-      </div> */}
-    </main>
-  );
-}
+    </div> */}
---
+++ 03-belajar-komponen/src/components/todolist.tsx
@@ -0,0 +1,17 @@
+export default function ToDoList(){
+    return(
+        <>
+        <h1>Daftar Tugas Putri</h1>
+        <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Putri"
+        className="photo" 
+        />
+        <ul>
+            <li>Mengerjakan PR</li>
+            <li>Pergi Belanja</li>
+            <li>Minum Vitamin</li>
+        </ul>
+        </>
+    );
+}
