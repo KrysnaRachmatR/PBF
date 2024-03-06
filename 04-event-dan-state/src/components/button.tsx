d13c43f47544af9867a2e34d8b0addd2c2943f2a
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-03-06 08:38:09 +0700

    Praktikum 1

---
+++ 04-event-dan-state/src/components/button.tsx
@@ -0,0 +1,22 @@
+export default function Tombol_1() {
+    function handleClick(){
+        alert("Tombol telah dipenek");
+      }
+      
+      function handleMouseOver(){
+        alert("Eits, mau ngaps ya?");
+      }
+    return(
+        <button 
+        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={handleClick}
+        // onMouseOver={handleMouseOver}
+        onMouseLeave={()=> {
+          alert("lahkok pergi?, ayo ngopi")
+        }
+      }
+        >
+            Ini Tombol
+        </button>
+    );
+}
