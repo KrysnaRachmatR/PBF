5c0e4ce766d29d02f462707f24bb9f15bdf7e422
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-03-27 18:57:00 +0700

    Praktikum 1

--- pertemuan-06/pages/_app.tsx
+++ pertemuan-06/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"; //install bootstrap
 import "@/styles/globals.css";
 import { useEffect } from "react";
-import exp from "constants";
 
-function myApp({Component, pageProps}) {
+
+function myApp({ Component, pageProps}) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   },[]);
