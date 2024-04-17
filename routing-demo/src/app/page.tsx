3145f7529c3358c161de02525a929838ad89f0b4
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-04-17 12:26:05 +0700

    Praktikum 4

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home(){
-    return <h1>Welcome to Home</h1>
+    return(
+        <div>
+            <h1>Welcome to Home</h1>
+            <Link href='/profile'>Profile</Link>
+            <br />
+            <Link href='/about'>About</Link>
+            <br />
+            <Link href='/product'>Product</Link>
+            <br />
+            <Link href='/blogs'>Blogs</Link>
+        </div>
+    )
 }
