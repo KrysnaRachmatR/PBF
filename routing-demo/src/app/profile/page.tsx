6e36d27ed40cfd7b6364fe60f377d8720f50fe88
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-04-17 12:08:01 +0700

    Praktikum 2

---
+++ routing-demo/src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile(){
+    return(
+    <div>
+         <h1>Hello, I'm Krysna Rachmat Ramadhan </h1> 
+         <h2>I am a Polynema student majoring in informatics engineering</h2>
+    </div>
+    );
+}
